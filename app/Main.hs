ed1715b6337dd9359c70d186e40354a5a47ae19d
Milan Vaško <m.vasko2@gmail.com>
2023-12-01 23:38:20 +0100

    Solve day 1b

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,72 @@
 module Main where
 
-import Data.Char (isDigit)
+import Data.Char (digitToInt, isDigit)
 import Data.Text qualified as T
 import Data.Text.Read qualified as T
 import Main.Utf8 (withUtf8)
-import Relude.Unsafe (fromJust)
+import Text.Regex.TDFA ((=~))
 
 main :: IO ()
 main = withUtf8 $ do
-    content <- decodeUtf8 <$> readFileBS "data/day1.txt"
-    let rawLines = lines $ T.pack content
-    let numbers = map (T.filter isDigit) rawLines
-    let firstAndLast = fromJust $ mapM getFirstAndLastChar numbers
-    let finalNumbers = fromJust $ mapM (textToInt . T.pack . charsToList) firstAndLast
-    print $ sum finalNumbers
+    content <- T.pack . decodeUtf8 <$> readFileBS "data/day1.txt"
+    calculate calculation1 content
+    calculate calculation2 content
 
--- TODO: refactor
+calculate :: (Text -> Maybe (Int, Int)) -> Text -> IO ()
+calculate calculation content = do
+    let rawLines = lines content
+    let foundNumbers = concatInts <<$>> mapM calculation rawLines
+    print $ sum <$> foundNumbers
+
+calculation1 :: Text -> Maybe (Int, Int)
+calculation1 = findFirstAndLastDigit . T.filter isDigit
+
+calculation2 :: Text -> Maybe (Int, Int)
+calculation2 = (liftMaybeFromTuple . mapTuple looseTextToInt) . (findFirstMatch &&& findLastMatch)
+
+needleRegex :: Text
+needleRegex = "one|two|three|four|five|six|seven|eight|nine|[1-9]"
+
+reverseNeedleRegex :: Text
+reverseNeedleRegex = "eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[1-9]"
+
+looseTextToInt :: Text -> Maybe Int
+looseTextToInt t = case t of
+    "one" -> Just 1
+    "two" -> Just 2
+    "three" -> Just 3
+    "four" -> Just 4
+    "five" -> Just 5
+    "six" -> Just 6
+    "seven" -> Just 7
+    "eight" -> Just 8
+    "nine" -> Just 9
+    x -> textToInt x
+
+findFirstMatch :: Text -> Text
+findFirstMatch = flip (=~) needleRegex
+
+findLastMatch :: Text -> Text
+findLastMatch = T.reverse . flip (=~) reverseNeedleRegex . T.reverse
+
 textToInt :: Text -> Maybe Int
 textToInt t = case T.decimal t of
     Right (n, _) -> Just n
     Left _ -> Nothing
 
-getFirstAndLastChar :: Text -> Maybe (Char, Char)
-getFirstAndLastChar text = case T.uncons text of
+findFirstAndLastDigit :: Text -> Maybe (Int, Int)
+findFirstAndLastDigit text = case T.uncons text of
     Just (first', rest) -> case T.unsnoc rest of
-        Just (_, last') -> Just (first', last')
-        Nothing -> Just (first', first')
+        Just (_, last') -> Just $ mapTuple digitToInt (first', last')
+        Nothing -> Just $ mapTuple digitToInt (first', first')
     Nothing -> Nothing
 
-charsToList :: (Char, Char) -> [Char]
-charsToList (a, b) = [a, b]
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple fn (a, b) = (fn a, fn b)
+
+liftMaybeFromTuple :: (Maybe a, Maybe b) -> Maybe (a, b)
+liftMaybeFromTuple (Just a, Just b) = Just (a, b)
+liftMaybeFromTuple (_, _) = Nothing
+
+concatInts :: (Int, Int) -> Int
+concatInts (a, b) = 10 * a + b
