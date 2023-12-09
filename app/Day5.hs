c0109b4414459adf9761a4edd2c85c606c73d40b
Milan Vaško <m.vasko2@gmail.com>
2023-12-09 12:57:54 +0100

    Refactor day 5 a bit

--- app/Day5.hs
+++ app/Day5.hs
@@ -1,6 +1,6 @@
 module Day5 where
 
--- TODO: REFACTOR EVERYTHING!!!
+-- TODO: refactor
 -- TODO: optimize
 
 import Text.Megaparsec (Parsec, chunk, parse, sepBy1)
@@ -12,84 +12,67 @@ import Util qualified as U
 filePath :: FilePath
 filePath = "data/day5.txt"
 
-data MappingDefinition s d = MappingDefinition
-    { destinationStart :: d
-    , sourceStart :: s
+data MappingDefinition = MappingDefinition
+    { destinationStart :: Int
+    , sourceStart :: Int
     , rangeLength :: Int
     }
     deriving stock (Show)
 
-newtype SeedIndex = SeedIndex Int
-    deriving newtype (Eq, Num, Ord, Show)
-newtype FertilizerIndex = FertilizerIndex Int
-    deriving newtype (Eq, Num, Ord, Show)
-newtype HumidityIndex = HumidityIndex Int
-    deriving newtype (Eq, Num, Ord, Show)
-newtype LightIndex = LightIndex Int
-    deriving newtype (Eq, Num, Ord, Show)
-newtype LocationIndex = LocationIndex Int
-    deriving newtype (Eq, Num, Ord, Show)
-newtype SoilIndex = SoilIndex Int
-    deriving newtype (Eq, Num, Ord, Show)
-newtype TemperatureIndex = TemperatureIndex Int
-    deriving newtype (Eq, Num, Ord, Show)
-newtype WaterIndex = WaterIndex Int
-    deriving newtype (Eq, Num, Ord, Show)
-
 data Almanac = Almanac
-    { seeds :: [SeedIndex]
-    , seedToSoilMap :: [MappingDefinition SeedIndex SoilIndex]
-    , soilToFertilizerMap :: [MappingDefinition SoilIndex FertilizerIndex]
-    , fertilizerToWaterMap :: [MappingDefinition FertilizerIndex WaterIndex]
-    , waterToLightMap :: [MappingDefinition WaterIndex LightIndex]
-    , lightToTemperatureMap :: [MappingDefinition LightIndex TemperatureIndex]
-    , temperatureToHumidityMap :: [MappingDefinition TemperatureIndex HumidityIndex]
-    , humidityToLocationMap :: [MappingDefinition HumidityIndex LocationIndex]
+    { seeds :: [Int]
+    , seedToSoilMap :: [MappingDefinition]
+    , soilToFertilizerMap :: [MappingDefinition]
+    , fertilizerToWaterMap :: [MappingDefinition]
+    , waterToLightMap :: [MappingDefinition]
+    , lightToTemperatureMap :: [MappingDefinition]
+    , temperatureToHumidityMap :: [MappingDefinition]
+    , humidityToLocationMap :: [MappingDefinition]
     }
     deriving stock (Show)
 
 run :: IO ()
-run = U.loadAndRun' filePath (calculate calculation2) (calculate calculation2)
+run = U.loadAndRun' filePath (calculate calculation1) (const "TODO")
 
 calculate :: (Almanac -> Int) -> Text -> Text
 calculate fn = U.prettyPrintParseError . fmap fn . parseAlmanac
 
 calculation1 :: Almanac -> Int
 calculation1 almanac =
-    foldr (\(LocationIndex item) accum -> min item accum) maxInt $
-        map (\seed' -> calculateIndex HumidityIndex (\(HumidityIndex ss) -> LocationIndex ss) seed' almanac.humidityToLocationMap) $
-            map (\seed' -> calculateIndex TemperatureIndex (\(TemperatureIndex ss) -> HumidityIndex ss) seed' almanac.temperatureToHumidityMap) $
-                map (\seed' -> calculateIndex LightIndex (\(LightIndex ss) -> TemperatureIndex ss) seed' almanac.lightToTemperatureMap) $
-                    map (\seed' -> calculateIndex WaterIndex (\(WaterIndex ss) -> LightIndex ss) seed' almanac.waterToLightMap) $
-                        map (\seed' -> calculateIndex FertilizerIndex (\(FertilizerIndex ss) -> WaterIndex ss) seed' almanac.fertilizerToWaterMap) $
-                            map (\seed' -> calculateIndex SoilIndex (\(SoilIndex ss) -> FertilizerIndex ss) seed' almanac.soilToFertilizerMap) $
-                                map (\seed' -> calculateIndex SeedIndex (\(SeedIndex ss) -> SoilIndex ss) seed' almanac.seedToSoilMap) $
+    foldr min maxInt $
+        map (calculateIndex almanac.humidityToLocationMap) $
+            map (calculateIndex almanac.temperatureToHumidityMap) $
+                map (calculateIndex almanac.lightToTemperatureMap) $
+                    map (calculateIndex almanac.waterToLightMap) $
+                        map (calculateIndex almanac.fertilizerToWaterMap) $
+                            map (calculateIndex almanac.soilToFertilizerMap) $
+                                map (calculateIndex almanac.seedToSoilMap) $
                                     almanac.seeds
 
 calculation2 :: Almanac -> Int
 calculation2 almanac =
-    foldr (\(LocationIndex item) accum -> min item accum) maxInt $
-        map (\seed' -> calculateIndex HumidityIndex (\(HumidityIndex ss) -> LocationIndex ss) seed' almanac.humidityToLocationMap) $
-            map (\seed' -> calculateIndex TemperatureIndex (\(TemperatureIndex ss) -> HumidityIndex ss) seed' almanac.temperatureToHumidityMap) $
-                map (\seed' -> calculateIndex LightIndex (\(LightIndex ss) -> TemperatureIndex ss) seed' almanac.lightToTemperatureMap) $
-                    map (\seed' -> calculateIndex WaterIndex (\(WaterIndex ss) -> LightIndex ss) seed' almanac.waterToLightMap) $
-                        map (\seed' -> calculateIndex FertilizerIndex (\(FertilizerIndex ss) -> WaterIndex ss) seed' almanac.fertilizerToWaterMap) $
-                            map (\seed' -> calculateIndex SoilIndex (\(SoilIndex ss) -> FertilizerIndex ss) seed' almanac.soilToFertilizerMap) $
-                                map (\seed' -> calculateIndex SeedIndex (\(SeedIndex ss) -> SoilIndex ss) seed' almanac.seedToSoilMap) $
+    foldr min maxInt $
+        map (calculateIndex almanac.humidityToLocationMap) $
+            map (calculateIndex almanac.temperatureToHumidityMap) $
+                map (calculateIndex almanac.lightToTemperatureMap) $
+                    map (calculateIndex almanac.waterToLightMap) $
+                        map (calculateIndex almanac.fertilizerToWaterMap) $
+                            map (calculateIndex almanac.soilToFertilizerMap) $
+                                map (calculateIndex almanac.seedToSoilMap) $
                                     (moreSeedsForCalculation2 almanac.seeds)
 
-moreSeedsForCalculation2 :: [SeedIndex] -> [SeedIndex]
+moreSeedsForCalculation2 :: [Int] -> [Int]
 moreSeedsForCalculation2 = moreSeedsForCalculation2' []
   where
-    moreSeedsForCalculation2' :: [SeedIndex] -> [SeedIndex] -> [SeedIndex]
+    moreSeedsForCalculation2' :: [Int] -> [Int] -> [Int]
     moreSeedsForCalculation2' accum [] = accum
     moreSeedsForCalculation2' _ [_] = error "TODO"
-    moreSeedsForCalculation2' accum (SeedIndex a : SeedIndex b : rest) = moreSeedsForCalculation2' (map SeedIndex [a .. a + b - 1] ++ accum) rest
+    moreSeedsForCalculation2' accum (a : b : rest) = moreSeedsForCalculation2' ([a .. a + b - 1] ++ accum) rest
 
-calculateIndex :: (Ord s, Num s, Num d) => (Int -> s) -> (s -> d) -> s -> [MappingDefinition s d] -> d
-calculateIndex sc sd source definitions = case filter (\d -> source >= d.sourceStart && source < d.sourceStart + sc d.rangeLength) definitions of
-    (x : _) -> sd source + (x.destinationStart - sd x.sourceStart)
-    _ -> sd source
+calculateIndex :: [MappingDefinition] -> Int -> Int
+calculateIndex definitions source = case filter (\d -> source >= d.sourceStart && source < d.sourceStart + d.rangeLength) definitions of
+    (x : _) -> source + (x.destinationStart - x.sourceStart)
+    _ -> source
 
 parseAlmanac :: Text -> Either (ParseErrorBundle Text Void) Almanac
 parseAlmanac = parse parser filePath
@@ -100,19 +83,19 @@ parser :: Parser Almanac
 parser = do
     seeds' <- seedsParser
     void $ many newline
-    seedToSoilMap' <- mapParser SeedIndex SoilIndex "seed-to-soil"
+    seedToSoilMap' <- mapParser "seed-to-soil"
     void $ many newline
-    soilToFertilizerMap' <- mapParser SoilIndex FertilizerIndex "soil-to-fertilizer"
+    soilToFertilizerMap' <- mapParser "soil-to-fertilizer"
     void $ many newline
-    fertilizerToWater' <- mapParser FertilizerIndex WaterIndex "fertilizer-to-water"
+    fertilizerToWater' <- mapParser "fertilizer-to-water"
     void $ many newline
-    waterToLight' <- mapParser WaterIndex LightIndex "water-to-light"
+    waterToLight' <- mapParser "water-to-light"
     void $ many newline
-    lightToTemperature' <- mapParser LightIndex TemperatureIndex "light-to-temperature"
+    lightToTemperature' <- mapParser "light-to-temperature"
     void $ many newline
-    temperatureToHumidity' <- mapParser TemperatureIndex HumidityIndex "temperature-to-humidity"
+    temperatureToHumidity' <- mapParser "temperature-to-humidity"
     void $ many newline
-    humidityToLocation' <- mapParser HumidityIndex LocationIndex "humidity-to-location"
+    humidityToLocation' <- mapParser "humidity-to-location"
 
     return $
         Almanac
@@ -126,27 +109,27 @@ parser = do
             , humidityToLocationMap = humidityToLocation'
             }
 
-seedsParser :: Parser [SeedIndex]
+seedsParser :: Parser [Int]
 seedsParser = do
     void (chunk "seeds")
     void (char ':')
     void hspace
-    map SeedIndex <$> decimal `sepBy1` hspace
+    decimal `sepBy1` hspace
 
-mapParser :: (Int -> s) -> (Int -> d) -> Text -> Parser [MappingDefinition s d]
-mapParser source dest name = do
+mapParser :: Text -> Parser [MappingDefinition]
+mapParser name = do
     void (chunk name)
     void hspace
     void (chunk "map")
     void (char ':')
     void newline
-    many (sourceDestinationRangeParser source dest)
+    many sourceDestinationRangeParser
 
-sourceDestinationRangeParser :: (Int -> s) -> (Int -> d) -> Parser (MappingDefinition s d)
-sourceDestinationRangeParser source dest = do
-    destinationRangeStart <- dest <$> decimal
+sourceDestinationRangeParser :: Parser MappingDefinition
+sourceDestinationRangeParser = do
+    destinationRangeStart <- decimal
     void hspace
-    sourceRangeStart <- source <$> decimal
+    sourceRangeStart <- decimal
     void hspace
     rangeLength <- decimal
     void newline
--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Day1 qualified
 import Day2 qualified
 import Day3 qualified
 import Day4 qualified
+import Day5 qualified
 import Day6 qualified
 import Day7 qualified
 import Day8 qualified
@@ -16,7 +17,7 @@ main = withUtf8 $ do
     runDay 2 Day2.run
     runDay 3 Day3.run
     runDay 4 Day4.run
-    -- runDay 5 Day5.run
+    runDay 5 Day5.run
     runDay 6 Day6.run
     runDay 7 Day7.run
     runDay 8 Day8.run
